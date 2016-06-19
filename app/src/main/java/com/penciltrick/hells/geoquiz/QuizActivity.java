61ccc4ade37b7975951e8af8b6f81e6bf0532f22
travis88 <hellsballs99@gmail.com>
2016-06-19 13:46:53 +0300

    second activity add

--- app/src/main/java/com/penciltrick/hells/geoquiz/QuizActivity.java
+++ app/src/main/java/com/penciltrick/hells/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.penciltrick.hells.geoquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,6 +21,7 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+    private Button mCheatButton;
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_oceans, true),
@@ -63,6 +65,15 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton = (Button) findViewById(R.id.false_button);
         mNextButton = (ImageButton) findViewById(R.id.next_button);
         mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivity(i);
+            }
+        });
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
